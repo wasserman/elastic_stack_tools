5f46d6ae1d7ccd0f4e15ec2a0dfcf12c36bc49ff
wasserman <brian-github2@starvingprogrammer.com>
2022-02-24 12:17:09 -0500

    Took out jq dependency.  Using Python json.
    Not all systems have jq.  
    Users may not have sudo or admin access.
    Python can do this but I wanted to avoid a requests module dependency.

--- backup_elastic_pipelines.sh
+++ backup_elastic_pipelines.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-for pipeline in `curl -s localhost:9200/_clsuter/state|jq .metadata.ingest.pipeline[].id -r`; do
-  echo $pipeline
-  curl -s localhost:9200/_cluster/state|jq ".metadata.ingest.pipeline[] | select(.id==\"$pipeline\") | .config" > $pipeline.pipeline.json
-done
+SERVER=https://localhost:9200
+
+# Assume .netrc is used for creds, so use curl "-n" option
+
+# Dump all pipelines into json via curl so we don't need requests.
+curl -nks $SERVER/_cluster/state?filter_path=metadata.ingest.pipeline > pipelines.json
+
+# Avoid use of jq since it isn't ubiquitous.  
+# Inline Python because I hope the json module is there and I don't know any other way.
+python <<HEREDOC
+import json, os
+pipelines = json.loads(open('pipelines.json','r').read())['metadata']['ingest']['pipeline']
+for pipeline in pipelines:
+  filename = '%s.pipeline.json' % pipeline['id']
+  print('Saving %s' % filename)
+  open(filename, 'w').write(json.dumps(pipeline['config'], indent=4, sort_keys=True))
+HEREDOC
